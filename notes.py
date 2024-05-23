f988a312da4a31622a6069b1fbdd47a4831cb8ba
Yiğit Tilaver <yigittilaver2000@gmail.com>
2024-05-23 22:42:03 +0200

    created notes.py file

---
+++ notes.py
@@ -0,0 +1,23 @@
+# Normal kodlama yaparken süreç içerisinde silinen kod parçacıkları:
+
+## the basic structure::
+# def myfunction():
+#   pass
+
+# mappings = {
+#   "Greeting": myfunction
+# }
+
+# assistant = GenericAssistant("intents.json", intent_methods=mappings)
+
+# assistant.train_model()
+
+# assistant.request("Hello")
+
+
+# portfolio = {"AAPL": 20 , "TSLA": 5, "AMZN": 3, "GOOGL": 2}
+
+# with open("stocks.pkl", "wb") as f:
+#     # wb = write binary
+#     pickle.dump(portfolio, f)
+
